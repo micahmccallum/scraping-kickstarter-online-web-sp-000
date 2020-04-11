9c449a93526f9a955c969eceab58b491ee1bf8d5
Micah McCallum <micahmccallum@outlook.com>
2020-04-11 20:54:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,11 +10,13 @@ def create_project_hash
   project_list = kickstarter.css(".project")
 
   project_list.each do |project|
-    image_link = project.css("img").attr("src").text.strip
-    description = project.css(".bbcard_blurb").text.strip
-    location = project.css(".location-name").text.strip
-    percent_funded = project.css(".funded strong").text.strip
-    projects[project] = {}
+    title = project.css(".bbcard_name strong").text.strip
+    projects[title.to_sym] = {
+    :image_link => project.css("img").attr("src").text.strip
+    :description => project.css(".bbcard_blurb").text.strip
+    :location => project.css(".location-name").text.strip
+    :percent_funded => project.css(".funded strong").text.strip
+  }
   end
   projects
   binding.pry
