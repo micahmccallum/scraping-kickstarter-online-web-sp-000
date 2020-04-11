c7f4cc3576fec5c104fbc2a9d85f52ecaea9b74c
Micah McCallum <micahmccallum@outlook.com>
2020-04-11 21:04:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,17 +7,17 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
+
   project_list = kickstarter.css(".project")
 
   project_list.each do |project|
     title = project.css(".bbcard_name strong").text.strip
     projects[title.to_sym] = {
-    :image_link => project.css("img").attr("src").text.strip,
-    :description => project.css(".bbcard_blurb").text.strip,
-    :location => project.css(".location-name").text.strip,
-    :percent_funded => project.css(".funded strong").text.to_i
-
-  }
+      :image_link => project.css("img").attr("src").text.strip,
+      :description => project.css(".bbcard_blurb").text.strip,
+      :location => project.css(".location-name").text.strip,
+      :percent_funded => project.css(".funded strong").text.to_i
+    }
   end
   projects
 end
