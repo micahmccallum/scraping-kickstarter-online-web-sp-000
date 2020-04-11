bcfb3307877c2c1f68a28380dd172c842f134837
Micah McCallum <micahmccallum@outlook.com>
2020-04-11 20:41:00 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ def create_project_hash
   project_list = kickstarter.css(".project")
 
   project_list.each do |project|
-    image_link = project.css("img").attr("src")
+    image_link = project.css("img").attr("src").text.strip
     description = project.css(".bbcard_blurb").text.strip
     location = project.css(".location-name").text.strip
     percent_funded = project.css(".funded strong").text.strip
